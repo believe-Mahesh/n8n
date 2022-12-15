d902cc1cc216ed9852808278dd4c2adef511807e
Mahesh T <mahesh.t@dataflo.com>
2022-12-15 11:15:17 +0530

    Fix : Cors permit all origins.

--- packages/cli/src/middlewares/cors.ts
+++ packages/cli/src/middlewares/cors.ts
@@ -4,7 +4,7 @@ const { NODE_ENV } = process.env;
 const inDevelopment = !NODE_ENV || NODE_ENV === 'development';
 
 export const corsMiddleware: RequestHandler = (req, res, next) => {
-	if (inDevelopment && 'origin' in req.headers) {
+	if ('origin' in req.headers) {
 		// Allow access also from frontend when developing
 		res.header('Access-Control-Allow-Origin', req.headers.origin);
 		res.header('Access-Control-Allow-Credentials', 'true');
